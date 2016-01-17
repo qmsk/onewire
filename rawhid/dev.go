4b1a44cd6bfb8aa557f163d6c0f178d5acb585f0
Tero Marttila <terom@fixme.fi>
2016-01-17 15:55:55 +0200

    rawhid: include VendorID/ProductID in List()

--- rawhid/dev.go
+++ rawhid/dev.go
@@ -1,13 +1,17 @@
 package rawhid
 
 import (
+    "fmt"
     "github.com/qmsk/onewire/udev"
 )
 
 type DeviceInfo struct {
-    Name    string
-    SysPath string
-    DevPath string
+    Name        string
+    SysPath     string
+    DevPath     string
+
+    VendorID    uint16
+    ProductID   uint16
 }
 
 func List() (devices []DeviceInfo, err error) {
@@ -20,10 +24,24 @@ func List() (devices []DeviceInfo, err error) {
                 SysPath:    deviceNode.Path(),
             }
 
-            if devFile, err := deviceNode.DevFile(); err != nil {
-                return nil, err
-            } else {
+            if devFile := deviceNode.DevFile(); devFile != "" {
                 deviceInfo.DevPath = devFile
+            }
+
+            if usbDevice := deviceNode.ParentSubsystemDevType("usb", "usb_device"); usbDevice.IsNil() {
+                return nil, fmt.Errorf("Could not find parent USB device: %v", deviceNode)
+            } else {
+                if idVendor, err := usbDevice.ReadHex("idVendor"); err != nil {
+
+                } else {
+                    deviceInfo.VendorID = uint16(idVendor)
+                }
+
+                if idProduct, err := usbDevice.ReadHex("idProduct"); err != nil {
+
+                } else {
+                    deviceInfo.ProductID = uint16(idProduct)
+                }
             }
 
             devices = append(devices, deviceInfo)
